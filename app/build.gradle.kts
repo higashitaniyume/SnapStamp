6caab87a79ded85e3feff22cb7e9871cfad2187c
higashitaniyume <higashitaniyume@gmail.com>
2026-04-14 12:47:04 +0800

    release: v0.1.2
    Add oil paint support 4 1ka

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "top.valency.snapstamp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
